338ee04f0a933b1fd86d0b6a5e112706d28a6245
codingkasia <codingkasia@gmail.com>
2018-11-28 14:38:50 -0500

    add sign-in logic

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+    before_action :authenticate
+    
     def index
         @todos = Todo.all
     end
@@ -13,6 +15,16 @@ class TodosController < ApplicationController
     end
 
     private
+
+    def authenticate
+        if !signed_in?
+            redirect_to new_session_path
+        end
+    end
+
+    def signed_in?
+        session[:current_email].present?
+    end
 
     def todo_params 
         params.require(:todo).permit(:title)
